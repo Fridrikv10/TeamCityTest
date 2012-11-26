9232fa624426084b8b4a7431ff9b150620077e2c
Fridrik <fridrikv10@ru.is>
2012-11-26 16:27:32 +0000

    Message

---
+++ src/MathD.java
@@ -0,0 +1,16 @@
+public class MathD {
+
+    public MathD(){
+
+    }
+
+	 public int add(int a, int b) {
+	
+		return a + b;
+	}
+	
+	 public int multiply ( int a, int b) {
+
+		return a * b;
+	}
+}
--- src/TestMath.java
+++ src/TestMath.java
@@ -2,6 +2,8 @@ import junit.framework.*;
 
 public class TestMath extends TestCase { 
 
+    public MathD math = new MathD();
+
   protected void setUp() { 
     	
   	// put common setup code in here
@@ -17,7 +19,7 @@ public class TestMath extends TestCase {
   	int num2 = 2;
   	int total = 5;
   	int sum = 0;
-  	sum = Math.add(num1, num2);
+  	sum = MathD.add(num1, num2);
   	assertEquals(sum, total);
   }
   
@@ -27,14 +29,14 @@ public class TestMath extends TestCase {
   	int num2 = 7; 
   	int total = 21;
   	int sum = 0;
-  	sum = Math.multiply(num1, num2);
+  	sum = MathD.multiply(num1, num2);
   	assertEquals("Problem with multiply", sum, total);
   	
   	
   	num1 = 5;
   	num2 = 4;
   	total = 20;
-  	sum = Math.multiply(num1, num2);
+  	sum = MathD.multiply(num1, num2);
   	assertEquals("Problem with multiply", sum, total);
   	
   }
